645e6ea2521a3be1ea8e4f80173decd4e87b2f53
Raozey <43946018+Raozey@users.noreply.github.com>
2023-05-01 21:56:12 +0800

    Create EarlyInitialization.cpp

---
+++ Singleton Pattern/EarlyInitialization.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+//饿汉模式(Early Initialization)
+class A {
+public:
+  A(const A&) = delete;  //禁用拷贝构造函数
+  A(const A&&) = delete; //禁用移动构造函数
+  A& operator = (const A&) = delete; //重载=禁止赋值
+  void func() {cout<<"hello, this is early initialization "<<endl}
+  static A* getInstance(){
+    return INSTANCE_;
+  }
+private:
+  A() = default;   //构造函数私有化,外部不能构造
+  ~A() = default;  //析构函数私有化保证单例对象无法被意外地销毁，只能在应用程序退出时由系统自动销毁
+  static A* INSTANCE_;
+}
+A* A::INSTANCE_ = new A;
+
+int main(){
+  A* obj = A::getInstance();
+  obj->func();
+}
